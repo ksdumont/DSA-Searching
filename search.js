795c1671ff133f80ca61924f7c210ba461b36a1f
ksdumont <ksdumont@gmail.com>
2020-02-29 09:02:22 -0800

    updated algo

--- search.js
+++ search.js
@@ -35,39 +35,44 @@ function binarySearch(array, value, start, end) {
 // Searching through trees, Depth-first search (DFS) / Breadth-first search (BFS)
 
 class BinarySearchTree {
-    constructor(key = null, value = null, parent = null) {
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
-        this.left = null;
-        this.right = null;
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+  dfs(values = []) {
+    if (this.left) {
+      values = this.left.dfs(values);
     }
-    dfs(values=[]) {
-        if (this.left) {
-            values = this.left.dfs(values)
-        }
-        values.push(this.value);
+    values.push(this.value);
 
-        if (this.right) {
-            values = this.right.dfs(values)
-        }
-        return values;
+    if (this.right) {
+      values = this.right.dfs(values);
     }
-    bfs(tree, values = []) {
-        const queue = new Queue();
-        const node = tree.root;
-        queue.enqueue(node);
-        while(queue.length) {
-            const node = queue.dequeue()
-            values.push(node.value);
+    return values;
+  }
+  bfs(tree, values = []) {
+    const queue = new Queue();
+    const node = tree.root;
+    queue.enqueue(node);
+    while (queue.length) {
+      const node = queue.dequeue();
+      values.push(node.value);
 
-            if (node.left) {
-                queue.enqueue(node.left)
-            }
-            if (node.right) {
-                queue.enqueue(node.right)
-            }
-        }
-        return values;
+      if (node.left) {
+        queue.enqueue(node.left);
+      }
+      if (node.right) {
+        queue.enqueue(node.right);
+      }
     }
+    return values;
+  }
 }
+
+// console.log(binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18], 8));
+// console.log(binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18], 16));
+
+
